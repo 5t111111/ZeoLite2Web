42535a5e551d8d8498715c15af75df6ea4c11d8e
Hirofumi Wakasugi <baenej@gmail.com>
2018-05-25 17:57:29 +0900

    Rename to ZeoLite2Web

--- Package.swift
+++ Package.swift
@@ -2,13 +2,13 @@
 import PackageDescription
 
 let package = Package(
-    name: "MMDBWebAPI",
+    name: "ZeoLite2Web",
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.3"),
-        .package(url: "https://github.com/5t111111/MaxMind.git", .branch("master"))
+        .package(url: "https://github.com/5t111111/ZeoLite2.git", .branch("master"))
     ],
     targets: [
-        .target(name: "App", dependencies: ["Vapor", "MaxMind"]),
+        .target(name: "App", dependencies: ["Vapor", "ZeoLite2"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
     ]
