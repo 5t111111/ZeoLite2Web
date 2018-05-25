cf9a6d0cae1bcc3e1ef4042d72a5b3c5d69ce6e3
Hirofumi Wakasugi <baenej@gmail.com>
2018-05-25 18:22:56 +0900

    Update database on boot

--- Sources/App/boot.swift
+++ Sources/App/boot.swift
@@ -3,4 +3,20 @@ import Vapor
 /// Called after your application has initialized.
 public func boot(_ app: Application) throws {
     // your code here
+    let directoryConfig = DirectoryConfig.detect()
+    let appRootPath = directoryConfig.workDir
+    let updateDBCommand = "\(appRootPath)update_database.sh"
+    print("=== Database update start (invoking \(updateDBCommand)) ===")
+    shell("sh", updateDBCommand)
+    print("=== Database update complete ===")
+}
+
+@discardableResult
+func shell(_ args: String...) -> Int32 {
+    let task = Process()
+    task.launchPath = "/usr/bin/env"
+    task.arguments = args
+    task.launch()
+    task.waitUntilExit()
+    return task.terminationStatus
 }
