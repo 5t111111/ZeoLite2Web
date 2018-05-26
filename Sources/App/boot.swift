dffab5d9fea4aaa692620da1a41b3365fb3a0623
Hirofumi Wakasugi <baenej@gmail.com>
2018-05-26 12:42:33 +0900

    Add env var to restrict DB update

--- Sources/App/boot.swift
+++ Sources/App/boot.swift
@@ -3,12 +3,15 @@ import Vapor
 /// Called after your application has initialized.
 public func boot(_ app: Application) throws {
     // your code here
-    let directoryConfig = DirectoryConfig.detect()
-    let appRootPath = directoryConfig.workDir
-    let updateDBCommand = "\(appRootPath)update_database.sh"
-    print("=== Database update start (invoking \(updateDBCommand)) ===")
-    shell("sh", updateDBCommand)
-    print("=== Database update complete ===")
+    if let _ = Environment.get("NO_DB_UPDATE") {
+    } else {
+        let directoryConfig = DirectoryConfig.detect()
+        let appRootPath = directoryConfig.workDir
+        let updateDBCommand = "\(appRootPath)update_database.sh"
+        print("=== Database update start (invoking \(updateDBCommand)) ===")
+        shell("sh", updateDBCommand)
+        print("=== Database update complete ===")
+    }
 }
 
 @discardableResult
