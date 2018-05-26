c16b8c8fcb298dd964db40fb86479201ac9ce68c
Hirofumi Wakasugi <baenej@gmail.com>
2018-05-26 12:42:15 +0900

    CORS configuration

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -9,7 +9,25 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
 
     /// Register middleware
     var middlewares = MiddlewareConfig() // Create _empty_ middleware config
+
+    // CORS
+    let corsConfig = CORSMiddleware.Configuration(
+        allowedOrigin: .originBased,
+        allowedMethods: [.GET, .POST, .PUT, .OPTIONS, .DELETE, .PATCH],
+        allowedHeaders: [.accept, .authorization, .contentType, .origin, .xRequestedWith, .userAgent],
+        exposedHeaders: [
+            HTTPHeaderName.authorization.description,
+            HTTPHeaderName.contentLength.description,
+            HTTPHeaderName.contentType.description,
+            HTTPHeaderName.contentDisposition.description,
+            HTTPHeaderName.cacheControl.description,
+            HTTPHeaderName.expires.description
+        ]
+    )
+    middlewares.use(CORSMiddleware(configuration: corsConfig))
+
     /// middlewares.use(FileMiddleware.self) // Serves files from `Public/` directory
     middlewares.use(ErrorMiddleware.self) // Catches errors and converts to HTTP response
+
     services.register(middlewares)
 }
