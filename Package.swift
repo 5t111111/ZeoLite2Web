1f2c8e0d4e16d190335c1fa0e59e693d430339ee
Hirofumi Wakasugi <baenej@gmail.com>
2018-05-27 13:17:51 +0900

    Update ZeoLite2 library

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.3"),
         .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0-rc.2.2"),
-        .package(url: "https://github.com/5t111111/ZeoLite2.git", .branch("master"))
+        .package(url: "https://github.com/5t111111/ZeoLite2.git", from: "1.0.0")
     ],
     targets: [
         .target(name: "App", dependencies: ["Vapor", "Leaf", "ZeoLite2"]),
