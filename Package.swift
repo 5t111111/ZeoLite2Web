82e8f1c90629274a418d8a8cc28655f90f59aba3
Hirofumi Wakasugi <baenej@gmail.com>
2018-05-27 13:40:32 +0900

    Add GraphQL package

--- Package.swift
+++ Package.swift
@@ -6,10 +6,11 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.3"),
         .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0-rc.2.2"),
-        .package(url: "https://github.com/5t111111/ZeoLite2.git", from: "1.0.0")
+        .package(url: "https://github.com/5t111111/ZeoLite2.git", from: "1.0.0"),
+        .package(url: "https://github.com/GraphQLSwift/GraphQL.git", from: "0.0.0"),
     ],
     targets: [
-        .target(name: "App", dependencies: ["Vapor", "Leaf", "ZeoLite2"]),
+        .target(name: "App", dependencies: ["Vapor", "Leaf", "ZeoLite2", "GraphQL"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
     ]
