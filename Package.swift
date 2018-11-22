22a2981f2f6cd84ad8cc7ec5949e320eed61e59c
Hirofumi Wakasugi <baenej@gmail.com>
2018-11-22 18:56:44 +0900

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -4,8 +4,8 @@ import PackageDescription
 let package = Package(
     name: "ZeoLite2Web",
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.3"),
-        .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0-rc.2.2"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.1.0"),
+        .package(url: "https://github.com/vapor/leaf.git", from: "3.0.2"),
         .package(url: "https://github.com/5t111111/ZeoLite2.git", from: "1.0.0")
     ],
     targets: [
